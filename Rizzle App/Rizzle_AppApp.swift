338f9c5f898c30d143f8a49a91a95da60f70ce8c
Johnny Sheffer <johnnysheffer@gmail.com>
2021-01-18 18:52:34 -0500

    Initial Commit

---
+++ Rizzle App/Rizzle_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rizzle_AppApp.swift
+//  Rizzle App
+//
+//  Created by Johnny Sheffer on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Rizzle_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
